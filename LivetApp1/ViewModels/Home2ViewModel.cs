3768ab78fcd54a3a614f4bfa777192dd59d06ab5
Yasukidauyo <hirayasuki@occ.co.jp>
2019-05-28 14:40:04 +0900

    add

--- LivetApp1/ViewModels/Home2ViewModel.cs
+++ LivetApp1/ViewModels/Home2ViewModel.cs
@@ -12,55 +12,159 @@ using Livet.EventListeners;
 using Livet.Messaging.Windows;
 
 using LivetApp1.Models;
-
+using System.Windows;
 namespace LivetApp1.ViewModels
 {
     public class Home2ViewModel : ViewModel
     {
-        /* コマンド、プロパティの定義にはそれぞれ 
-         * 
-         *  lvcom   : ViewModelCommand
-         *  lvcomn  : ViewModelCommand(CanExecute無)
-         *  llcom   : ListenerCommand(パラメータ有のコマンド)
-         *  llcomn  : ListenerCommand(パラメータ有のコマンド・CanExecute無)
-         *  lprop   : 変更通知プロパティ(.NET4.5ではlpropn)
-         *  
-         * を使用してください。
-         * 
-         * Modelが十分にリッチであるならコマンドにこだわる必要はありません。
-         * View側のコードビハインドを使用しないMVVMパターンの実装を行う場合でも、ViewModelにメソッドを定義し、
-         * LivetCallMethodActionなどから直接メソッドを呼び出してください。
-         * 
-         * ViewModelのコマンドを呼び出せるLivetのすべてのビヘイビア・トリガー・アクションは
-         * 同様に直接ViewModelのメソッドを呼び出し可能です。
-         */
-
-        /* ViewModelからViewを操作したい場合は、View側のコードビハインド無で処理を行いたい場合は
-         * Messengerプロパティからメッセージ(各種InteractionMessage)を発信する事を検討してください。
-         */
-
-        /* Modelからの変更通知などの各種イベントを受け取る場合は、PropertyChangedEventListenerや
-         * CollectionChangedEventListenerを使うと便利です。各種ListenerはViewModelに定義されている
-         * CompositeDisposableプロパティ(LivetCompositeDisposable型)に格納しておく事でイベント解放を容易に行えます。
-         * 
-         * ReactiveExtensionsなどを併用する場合は、ReactiveExtensionsのCompositeDisposableを
-         * ViewModelのCompositeDisposableプロパティに格納しておくのを推奨します。
-         * 
-         * LivetのWindowテンプレートではViewのウィンドウが閉じる際にDataContextDisposeActionが動作するようになっており、
-         * ViewModelのDisposeが呼ばれCompositeDisposableプロパティに格納されたすべてのIDisposable型のインスタンスが解放されます。
-         * 
-         * ViewModelを使いまわしたい時などは、ViewからDataContextDisposeActionを取り除くか、発動のタイミングをずらす事で対応可能です。
-         */
-
-        /* UIDispatcherを操作する場合は、DispatcherHelperのメソッドを操作してください。
-         * UIDispatcher自体はApp.xaml.csでインスタンスを確保してあります。
-         * 
-         * LivetのViewModelではプロパティ変更通知(RaisePropertyChanged)やDispatcherCollectionを使ったコレクション変更通知は
-         * 自動的にUIDispatcher上での通知に変換されます。変更通知に際してUIDispatcherを操作する必要はありません。
-         */
-         
+      
         public void Initialize()
         {
         }
+
+        #region ShowKejiban
+        private ViewModelCommand _ShowJireiCommand;
+
+        public ViewModelCommand ShowJireiCommand
+
+        {
+            get
+            {
+                if (_ShowJireiCommand == null)
+                {
+                    _ShowJireiCommand = new ViewModelCommand(ShowJirei);
+                }
+                return _ShowJireiCommand;
+            }
+        }
+
+        public void ShowJirei()
+        {
+            System.Diagnostics.Debug.WriteLine("ShowJirei");
+            var window = Application.Current.Windows.OfType<Window>().SingleOrDefault((w) => w.IsActive);
+
+            try
+            {
+                // MainWindow を非表示
+                window.Hide();
+                JireiViewModel ViewModel = new JireiViewModel();
+                var message = new TransitionMessage(typeof(Views.jirei), ViewModel, TransitionMode.Modal, "ShowJirei");
+                Messenger.Raise(message);
+            }
+            finally
+            {
+                // MainWindow を再表示
+                window.ShowDialog();
+            }
+        }
+        #endregion
+        #region ShowGyoumukaizen
+        private ViewModelCommand _ShowgyoumukaizenCommand;
+
+        public ViewModelCommand ShowgyoumukaizenCommand
+
+        {
+            get
+            {
+                if (_ShowgyoumukaizenCommand == null)
+                {
+                    _ShowgyoumukaizenCommand = new ViewModelCommand(Showgyoumukaizen);
+                }
+                return _ShowgyoumukaizenCommand;
+            }
+        }
+
+        public void Showgyoumukaizen()
+        {
+            System.Diagnostics.Debug.WriteLine("Showgyoumukaizen");
+            var window = Application.Current.Windows.OfType<Window>().SingleOrDefault((w) => w.IsActive);
+
+            try
+            {
+                // MainWindow を非表示
+                window.Hide();
+                gyoumukaizenViewModel ViewModel = new gyoumukaizenViewModel();
+                var message = new TransitionMessage(typeof(Views.gyoumukaizen), ViewModel, TransitionMode.Modal, "Showgyoumukaizen");
+                Messenger.Raise(message);
+            }
+            finally
+            {
+                // MainWindow を再表示
+                window.ShowDialog();
+            }
+        }
+        #endregion
+        #region ShowTourokugamen
+        private ViewModelCommand _ShowTourokugamenCommand;
+
+        public ViewModelCommand ShowTourokugamenCommand
+
+        {
+            get
+            {
+                if (_ShowTourokugamenCommand == null)
+                {
+                    _ShowTourokugamenCommand = new ViewModelCommand(ShowTourokugamen);
+                }
+                return _ShowTourokugamenCommand;
+            }
+        }
+
+        public void ShowTourokugamen()
+        {
+            System.Diagnostics.Debug.WriteLine("ShowTourokugamen");
+            var window = Application.Current.Windows.OfType<Window>().SingleOrDefault((w) => w.IsActive);
+
+            try
+            {
+                // MainWindow を非表示
+                window.Hide();
+                TourokugamenViewModel ViewModel = new TourokugamenViewModel();
+                var message = new TransitionMessage(typeof(Views.tourokugamen), ViewModel, TransitionMode.Modal, "ShowTourokugamen");
+                Messenger.Raise(message);
+            }
+            finally
+            {
+                // MainWindow を再表示
+                window.ShowDialog();
+            }
+        }
+        #endregion
+        #region ShowSanshou
+        private ViewModelCommand _ShowSanshouCommand;
+
+        public ViewModelCommand ShowSanshouCommand
+
+        {
+            get
+            {
+                if (_ShowSanshouCommand == null)
+                {
+                    _ShowSanshouCommand = new ViewModelCommand(ShowSanshou);
+                }
+                return _ShowSanshouCommand;
+            }
+        }
+
+        public void ShowSanshou()
+        {
+            System.Diagnostics.Debug.WriteLine("ShowSanshou");
+            var window = Application.Current.Windows.OfType<Window>().SingleOrDefault((w) => w.IsActive);
+
+            try
+            {
+                // MainWindow を非表示
+                window.Hide();
+                SanshouViewModel ViewModel = new SanshouViewModel();
+                var message = new TransitionMessage(typeof(Views.Sanshou), ViewModel, TransitionMode.Modal, "ShowSanshou");
+                Messenger.Raise(message);
+            }
+            finally
+            {
+                // MainWindow を再表示
+                window.ShowDialog();
+            }
+        }
+        #endregion
     }
 }
